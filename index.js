5c3eb530cd5e3a31bca5816c8df76c196927689b
Wenderson <wps713@gmail.com>
2019-10-15 12:55:56 -0300

    Criando projetos estáticos

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const server = express()
 
 server.listen(3000)
 server.use(express.json())
+
+
+const projects = [
+  {
+    id: "1",
+    title: "Novo projeto",
+    tasks: ["Nova tarefa"]
+  },
+  {
+    id: "2",
+    title: "Projeto sensacional",
+    tasks: ["Reunião com equipe", "Planejar roadmap"]
+  }
+]
